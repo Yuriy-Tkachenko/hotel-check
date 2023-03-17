4f437afd190287f2054dcbbe75e4084936bf60bf
Yuriy-Tkachenko <apolo16@inbox.ru>
2023-03-17 23:17:39 +0300

    added images, created mocks data, created theme for app

---
+++ src/mocks/hotels.js
@@ -0,0 +1,34 @@
+import house from "../assets/house.svg";
+
+const hotels = [
+  {
+    id: 0,
+    title: "Moscow Marriott Grand Hotel",
+    image: house,
+    price: "23 924 ₽"
+  },
+  {
+    id: 1,
+    title: "Soluxe Hotel Moscow",
+    image: house,
+    price: "18 050 ₽"
+  },
+  {
+    id: 2,
+    title: "Pentahotel Москва Арбат",
+    image: house,
+    price: "21 683 ₽"
+  },
+  {
+    id: 3,
+    title: "Moscow Holiday",
+    image: house,
+    price: "16 321 ₽"
+  },
+  {
+    id: 4,
+    title: "Mamaison All-Suites Spa Hotel",
+    image: house,
+    price: "19 100 ₽"
+  }
+]
---
+++ src/thems/default.js
@@ -0,0 +1,10 @@
+export const defaultTheme = {
+  blackColor: "#424242",
+  lightgreyColor: "#C9CACC",
+  darkGreenColor: "#41522E",
+  indent: "24px",
+  fontFamily: '"Roboto", "Arial", sans-serif',
+  fontSizeDefault: "16px",
+  fontWeightDefault: "300",
+  pageWidth: "1048px"
+}
